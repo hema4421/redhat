213b6cc33362d7310095bfd108cde4d18dd7d91b
hema4421 <hemalatha4421@gmail.com>
2021-10-19 00:42:22 +0530

    commit

---
+++ src/test/java/com/example/demo/Azure2azure2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Azure2azure2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
